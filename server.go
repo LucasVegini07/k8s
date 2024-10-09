50ba243891a75199b3306b5e24adabeaa29379d3
Lucas Vegini <lucasramthum@gmail.com>
2024-10-08 21:57:04 -0300

    feat: create first pod

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":80", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("<h1> Hello World!</h1>"))
+}
