b78d2bb2a5d31d927c88ac1873961bb8a33f03ae
Lucas Vegini <lucasramthum@gmail.com>
2024-10-09 17:51:49 -0300

    feat: add environment variable

--- server.go
+++ server.go
@@ -8,5 +8,10 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+
     w.Write([]byte("<h1> Hello World!</h1>"))
 }
